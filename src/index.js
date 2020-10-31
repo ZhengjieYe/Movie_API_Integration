8458b24aed791dbb2e35205a53e1f55adb2fc161
ZhengjieYe <20091571@mail.wit.ie>
2020-10-31 19:38:13 +0000

    Added Upcoming movies page

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import HomePage from "./pages/homePage";
 import MoviePage from './pages/movieDetailsPage'
 import FavoriteMoviesPage from './pages/favoritesMoviesPage' 
 import MovieReviewPage from "./pages/movieReviewPage";
-import SiteHeader from './components/siteHeader'
+import SiteHeader from './components/siteHeader';
+import UpcomingMoviePage from './pages/movieUpcomingPage';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <SiteHeader />
       <div className="container-fluid">
         <Switch>
+          <Route exact path="/movies/upcoming" component={UpcomingMoviePage}/>
           <Route path="/reviews/:id" component={MovieReviewPage} />
           <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
           <Route path="/movies/:id" component={MoviePage} />
