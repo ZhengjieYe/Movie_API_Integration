8ee334e1c6f98f08ca46cf34df13695ca6c40c76
ZhengjieYe <20091571@mail.wit.ie>
2020-10-31 21:58:31 +0000

    E2E tests - Home page - Filtering by title - Exceptional case

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -50,6 +50,11 @@ describe("Home Page ", () => {
           .should("have.text", matchingMovies[index].title);
         })
       })
+      it("should not display any movies with 'xyz' in the title",() => {
+        const searchString = "xyz";
+        cy.get("input").clear().type(searchString);
+        cy.get(".card").should("have.length",0);
+      })
     })
     describe("By movie genre" ,() => {
       // More later
