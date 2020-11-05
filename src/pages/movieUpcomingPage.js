a26629006ba15b351742119be2a55eceb2426768
ZhengjieYe <20091571@mail.wit.ie>
2020-11-05 23:28:25 +0000

    New button for upcoming movies page

--- src/pages/movieUpcomingPage.js
+++ src/pages/movieUpcomingPage.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import PageTemplate from '../components/templateMovieListPage'
 import {MoviesContext} from '../contexts/moviesContext'
-import AddToFavoritesButton from '../components/buttons/addToFavorites'
+import AddToWatchListButton from '../components/buttons/addToWatchList'
 
 const MovieListPage = () => {
   const context = useContext(MoviesContext);
@@ -14,7 +14,7 @@ const MovieListPage = () => {
         title='Upcomming Movies'
         movies={movies}
         action={(movie) => {
-          return <AddToFavoritesButton movie={movie} /> 
+          return <AddToWatchListButton movie={movie} /> 
         }}
       />
   );
