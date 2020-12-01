e5d010070c7b6833e1ed6d45c14d800f488419fd
ZhengjieYe <20091571@mail.wit.ie>
2020-12-01 21:34:08 +0000

    Agile-feat: add code-coverage

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,6 +16,14 @@
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  require('@cypress/code-coverage/task')(on, config)
+  
+  // add other tasks to be registered here
+  on(
+    'file:preprocessor',
+    require('@cypress/code-coverage/use-browserify-istanbul')
+  )
+  // IMPORTANT to return the config object
+  // with the any changed environment variables
+  return config
 }
