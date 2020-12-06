9bf5841ba30c02ce2e4c62a1ea450ef3c97cb420
ZhengjieYe <20091571@mail.wit.ie>
2020-12-06 13:42:01 +0000

    Web-refactor: refactor page-template components

--- src/components/movieList/index.js
+++ src/components/movieList/index.js
@@ -6,7 +6,7 @@ const MovieList = ({movies, action}) => {
   const movieCards = movies.map(m => (
     <Movie key={m.id} movie={m} action={action} />
   ));
-  return <div className="row movies bg-info">{movieCards}</div>;
+  return <div className="row movies w-100 m-0" style={{backgroundColor:"black"}}>{movieCards}</div>;
 };
 
 export default MovieList;
