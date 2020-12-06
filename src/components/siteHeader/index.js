c2e66ba6c5a148393c7700de11c7503bd4ee88ee
ZhengjieYe <20091571@mail.wit.ie>
2020-12-06 13:28:25 +0000

    Web-refactor: refactor siteHead

--- src/components/siteHeader/index.js
+++ src/components/siteHeader/index.js
@@ -1,30 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../globals/fontawesome";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./siteHeader.css";
 
 export const SiteHeader = () => {
   return (
-    <nav className="navbar  navbar-light fixed-top  bg-dark ">
-      <nav className="navbar-brand text-white">
+    <nav className="navbar fixed-top" style={{backgroundColor:"rgba(0,0,0,0.3)"}}>
+      <nav className="navbar-brand">
         <Link className=" text-white" to="/">
           TMDB Client
         </Link>
       </nav>
-      <FontAwesomeIcon
-        className="navbar-text text-light"
-        icon={["fas", "video"]}
-        size="3x"
-      />
-      <span className="navbar-text text-light">
-        For the movie enthusiast !!
-      </span>
-      <FontAwesomeIcon
-        className="navbar-text text-light"
-        icon={["fas", "film"]}
-        size="3x"
-      />
+
       <nav className="navbar navbar-expand ">
         <ul className="navbar-nav">
           <li className="nav-item">
