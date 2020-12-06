45ef38be365e1a1b781ab7eaf6dd680709b01f56
ZhengjieYe <20091571@mail.wit.ie>
2020-12-06 18:27:26 +0000

    Agile-refactor: refactor navigation test

--- src/pages/movieUpcomingPage.js
+++ src/pages/movieUpcomingPage.js
@@ -2,18 +2,31 @@ import React, { useContext } from "react";
 import PageTemplate from '../components/templateMovieListPage'
 import {MoviesContext} from '../contexts/moviesContext'
 import AddToWatchListButton from '../components/buttons/addToWatchList'
+import ComboButton from '../components/buttons/comboButton'
 
 const MovieListPage = () => {
   const context = useContext(MoviesContext);
 
-  const upcomingMovies = context.upcoming.filter((u) => !u.isInWatchList)
+  const upcomingMovies = context.upcoming;
 
   return (
       <PageTemplate 
         title='Upcoming Movies'
         movies={upcomingMovies}
+        carouselAction={(item) => {
+          return <ComboButton
+           item={item} 
+           removeAlertAction={context.removeFromWatchlist} 
+           addButtonAction={context.addToWatchList} 
+           content="Watchlist"
+          />
+        }}
         action={(movie) => {
-          return <AddToWatchListButton movie={movie} /> 
+          return <AddToWatchListButton 
+            movie={movie} 
+            removeAlertAction={context.removeFromWatchlist} 
+            addButtonAction={context.addToWatchList}
+          /> 
         }}
       />
   );
