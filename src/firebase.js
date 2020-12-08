e0091ad5c43a942da6b3a737d4a6732c9084cfd9
ZhengjieYe <20091571@mail.wit.ie>
2020-12-08 22:44:17 +0000

    Web-feat: add firebase and according setting

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCIGlcX9S75PGsEFZwBO5FmTINf2AFCTDg",
+    authDomain: "movieapp-30589.firebaseapp.com",
+    projectId: "movieapp-30589",
+    storageBucket: "movieapp-30589.appspot.com",
+    messagingSenderId: "246577744348",
+    appId: "1:246577744348:web:da3dba93aab14161362719",
+    measurementId: "G-VFF43CX0PQ"
+  };
+
+export default firebaseConfig;
--- src/index.js
+++ src/index.js
@@ -11,33 +11,37 @@ import UpcomingMoviePage from './pages/movieUpcomingPage';
 import MoviesContextProvider from "./contexts/moviesContext";
 import GenresContextProvider from "./contexts/genresContext";
 import AddMovieReviewPage from './pages/addMovieReviewPage';
+import { FirebaseAppProvider } from 'reactfire';
+import firebaseConfig from './firebase';
 
 const App = () => {
   return (
-    <BrowserRouter>
-    <div className="jumbotron p-0 m-0 w-100">
-      <SiteHeader />
-      <div className="container-fluid p-0">
-        <MoviesContextProvider>
-          <GenresContextProvider>
-            <Switch>
-              <Route exact path="/reviews/form" component={AddMovieReviewPage} />
-              <Route exact path="/movies/upcoming" component={UpcomingMoviePage}/>
-              <Route path="/reviews/:id" component={MovieReviewPage} />
-              <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
-              <Route path="/movies/:id" component={MoviePage} />
-              <Route path="/" render={()=>{
-                return (
-                  <HomePage className="w-100"/>
-                );
-              }}/>
-              <Redirect from="*" to="/" />
-            </Switch>
-          </GenresContextProvider>
-        </MoviesContextProvider>
-      </div>
-    </div>
-  </BrowserRouter>
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <BrowserRouter>
+        <div className="jumbotron p-0 m-0 w-100">
+          <SiteHeader />
+          <div className="container-fluid p-0">
+            <MoviesContextProvider>
+              <GenresContextProvider>
+                <Switch>
+                  <Route exact path="/reviews/form" component={AddMovieReviewPage} />
+                  <Route exact path="/movies/upcoming" component={UpcomingMoviePage}/>
+                  <Route path="/reviews/:id" component={MovieReviewPage} />
+                  <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
+                  <Route path="/movies/:id" component={MoviePage} />
+                  <Route path="/" render={()=>{
+                    return (
+                      <HomePage className="w-100"/>
+                    );
+                  }}/>
+                  <Redirect from="*" to="/" />
+                </Switch>
+              </GenresContextProvider>
+            </MoviesContextProvider>
+          </div>
+        </div>
+      </BrowserRouter>
+    </FirebaseAppProvider>
   );
 };
 
